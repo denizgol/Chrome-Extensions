1013fe7b0099e7135819df17999becefc601dd3f
Deniz Göl <60497542+denizgol@users.noreply.github.com>
2022-04-17 11:36:03 +0300

    Update popup.js

--- random number generator/popup.js
+++ random number generator/popup.js
@@ -4,7 +4,7 @@ function generate() {
  
       var min= document.getElementById("minkutu").value;
       var max= document.getElementById("maxkutu").value;
-      if(max<9999999 && min<9999999){
+      if(max<9999999 && min<9999999 && min==110 && min==188 && min==32 && max==110 && max==188 && max==32){
 
       if(min==max){
          max++;
@@ -17,7 +17,7 @@ function generate() {
         $('#result').text(Math.floor(Math.random() * (max - min + 1) ) - (-min));
     }
 
-    }else{alert("Please enter value less than <9999999 you're insane"  
+    }else{alert("Please enter value less than <9999999 or just use numbers"  
     
     )};
 }

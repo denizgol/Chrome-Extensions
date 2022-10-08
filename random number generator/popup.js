5a54c53775da9fdd4295fe85676d0f337e2e57d6
Deniz Göl <60497542+denizgol@users.noreply.github.com>
2022-10-09 00:46:19 +0300

    fix some problems

--- random number generator/popup.js
+++ random number generator/popup.js
@@ -4,7 +4,7 @@ function generate() {
  
       var min= document.getElementById("minkutu").value;
       var max= document.getElementById("maxkutu").value;
-      if(max<9999999 && min<9999999 && min==110 && min==188 && min==32 && max==110 && max==188 && max==32){
+      if(max<9999999 && min<9999999 && min!=110 && min!=188 && min!=32 && max!=110 && max!=188 && max!=32){
 
       if(min==max){
          max++;
